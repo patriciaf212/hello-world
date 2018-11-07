cf2629a97afea9d0f774778f618610d71d1f5c4d
Patricia Falar <patriciafalar212@gmail.com>
2018-11-07 10:49:25 -0500

    add page

---
+++ file.js
@@ -0,0 +1,11 @@
+function sayConsole () {
+  var p = document.getElementbyId (h)
+}
+function sayBrowser () {
+	var p = document.getElementById ("hello");
+	p.innerHTML = "Hello, World!";
+}
+function sayPopup () {
+  alert("Hello, World");
+}
+function say
